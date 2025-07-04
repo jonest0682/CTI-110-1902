d737fead61c229d031c4fdc061390d9d00ae32ec
jonest0682 <jonest0682@student.faytechcc.edu>
2025-07-03 23:40:10 -0400

    Add files via upload
    P1LAB1 submission. First upload.

---
+++ P1LAB_JonesTimothy.py
@@ -0,0 +1,20 @@
+# Timothy Jones
+# 7/3/2025
+# P1LAB1
+# Introduction to Python assignment
+
+
+
+# My Name
+
+Fname = "Timothy"
+Lname = "Jones"
+FullName = "Timothy\tJones"
+
+# Print out sentence
+
+print("Enter your first name:", Fname)
+print("Enter your last name:", Lname)
+print("Hello,", FullName, "!", "Welcome to CTI-110")
+
+# End of assignment
