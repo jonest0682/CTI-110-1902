73f57c7508c712f303cac1c6954f6361b05b0937
jonest0682 <jonest0682@student.faytechcc.edu>
2025-07-05 22:11:58 -0400

    Add files via upload

---
+++ P1HW1_JonesTimothy.py
@@ -0,0 +1,41 @@
+# Timothy Jones
+# 7/5/2025
+# P1HW1
+# Assignment tests student's knowledge of how to write code that collects information from user,
+# processes information collected and display results to user
+
+#First Line
+#Header
+print("-----Calculating Exponents -----\n\n")
+
+#Integer values
+
+baseValue = int(9)
+exponent = int(3)
+total = baseValue ** int(3)
+
+#Print results
+
+print("Enter an integer as the base value:", 9)
+print("Enter an integer as the exponent:", 3, "\n")
+print(baseValue,"raised to the power of ", exponent, "is ", total, " !!" "\n")
+
+
+#Header 2,
+
+print("-----Addition and Subtraction -----\n\n")
+
+#Integer values
+
+NewbaseValue = int(15)
+addValue = int(6)
+subValue = int(4)
+total = NewbaseValue + addValue - subValue
+
+#Print results
+
+print("Enter an starting integer: ", 15)
+print("Enter an integer to add: ", 6)
+print("Enter an integer to subtract: ", 4, "\n")
+print(NewbaseValue, "+ ", addValue, "- ", subValue, "is equal to ", total)
+#Last line
